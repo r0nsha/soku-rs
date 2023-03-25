a3ffd67f80204bc4054833483e50646cf2f21f6f
r0nsha <r0nsh4v1t@gmail.com>
2023-03-25 19:45:42 +0300

    output json

--- examples/create_sudokus_file.rs
+++ examples/create_sudokus_file.rs
@@ -1,10 +1,10 @@
 use std::{
     fs,
-    io::Write,
     sync::atomic::{AtomicUsize, Ordering},
 };
 
 use futures::{future, stream::FuturesUnordered};
+use serde_json::json;
 use soku::{measure, prelude::*};
 
 static PROGRESS: AtomicUsize = AtomicUsize::new(0);
@@ -17,22 +17,34 @@ async fn main() {
         let futures = (0..TOTAL)
             .map(|_| {
                 tokio::spawn(async {
-                    let result = Sudoku::new_unique(SudokuConfig { cells: CELLS }).to_str_line();
+                    let config = SudokuConfig { cells: CELLS };
+                    let filled = Sudoku::new_filled(config);
+                    let sudoku = LatinSquares.generate_from(filled.clone(), config);
+                    // let result = Sudoku::new_unique(SudokuConfig { cells: CELLS }).to_str_line();
                     PROGRESS.fetch_add(1, Ordering::Relaxed);
                     println!("progress: {PROGRESS:?}/{TOTAL}");
-                    result
+                    (filled, sudoku)
                 })
             })
             .collect::<FuturesUnordered<_>>();
 
         let results = future::join_all(futures).await;
 
-        let mut file = fs::File::create("sudokus.txt").unwrap();
+        let jsons = results
+            .into_iter()
+            .map(|result| {
+                let (filled, sudoku) = result.unwrap();
+                let (filled, sudoku) = (filled.to_str_line(), sudoku.to_str_line());
 
-        for result in results {
-            let str = result.unwrap();
-            // file.write_all(str.as_bytes()).unwrap();
-            writeln!(file, "{}", str).unwrap();
-        }
+                json!({
+                    "filled": filled,
+                    "sudoku": sudoku
+                })
+            })
+            .collect::<Vec<_>>();
+
+        let entire_json = serde_json::to_string_pretty(&jsons).unwrap();
+
+        fs::write("sudokus.json", entire_json).unwrap();
     });
 }
